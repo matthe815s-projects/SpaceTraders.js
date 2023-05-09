749b63ea0bfe7d41f2aa714a0460f56ccb2c9163
Matthe815 <matthew.elswick@matthe815.dev>
2023-05-09 18:38:47 -0400

    refactor: Begin refactoring into base library

--- build/src/client/ships/ShipStatus.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ShipStatus = void 0;
-/* eslint-disable no-unused-vars */
-var ShipStatus;
-(function (ShipStatus) {
-    ShipStatus["IN_ORBIT"] = "IN_ORBIT";
-    ShipStatus["DOCKED"] = "DOCKED";
-})(ShipStatus = exports.ShipStatus || (exports.ShipStatus = {}));
--- build/src/web/pages/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Index(client) {
-    return function (req, res) { return res.render('index', { ships: ['test'] }); };
-}
-exports.default = Index;
