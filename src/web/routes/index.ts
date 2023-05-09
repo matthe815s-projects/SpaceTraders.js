6bb46a6f334eb45636de8e4ec49709496d91fbba
Matthe815 <matthew.elswick@matthe815.dev>
2023-05-08 23:55:36 -0400

    dependencies: added react and react dom types

--- src/web/routes/index.ts
+++ src/web/routes/index.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express'
 import Client from '../../client/Client'
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 
 export default function Index (client: Client) {
   console.log("index")
-  return (req: Request, res: Response) => res.send(`Hello World! Currently running with ${client.token}`)
+  return (req: Request, res: Response) => res.send(ReactDomServer.renderToString(<App />)
 }
