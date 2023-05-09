b06a182f3b4dea5ec9fea7daad825acc6d5b6ebc
Matthe815 <matthew.elswick@matthe815.dev>
2023-05-08 23:42:29 -0400

    feature: Express listener and basic route

--- src/web/WebClient.ts
+++ src/web/WebClient.ts
@@ -1,9 +1,21 @@
-import Client from '../client/Client';
+import Client from '../client/Client'
+import Index from './routes'
+const express = require('express')
 
 export default class WebClient {
   private client: Client
+  public express: any
 
   constructor (client: Client) {
     this.client = client
+    this.express = express()
+
+    this.express.listen(3000, () => console.log('Listening on port 3000'))
+
+    this.registerEndpoints()
+  }
+
+  registerEndpoints () {
+    this.express.get('/', Index)
   }
 }
